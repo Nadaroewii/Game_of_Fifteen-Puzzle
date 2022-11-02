af4fb3f743b1bb424cba3c19bae570f9f84df607
Nadaroewii <nadaroewii@gmail.com>
2022-11-02 17:35:46 +0700

    up file

---
+++ app/src/main/java/com/lat1/nadavsga/MainActivity.java
@@ -0,0 +1,26 @@
+package com.lat1.nadavsga;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void Start(View view) {
+        Intent intent = new Intent(MainActivity.this, Menu.class);
+        startActivity(intent);
+    }
+}
